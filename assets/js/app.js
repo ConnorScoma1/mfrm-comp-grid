c4815b7b1cc4181776b7778a96f2cf5d0a0f9030
connor scoma <connorscoma@connors-MacBook-Pro.local>
2019-06-08 00:16:04 -0500

    adding cheerio

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,27 @@
-var axios = require('axios');
+var fs = require('fs');
+var axios = require('axios')
 var cheerio = require('cheerio')
+
+const url = 'https://news.ycombinator.com';
+
+axios.get(url)
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.log(error);
+  })
+
+let getData = html => {
+    data = [];
+    const $ = cheerio.load(html);
+    $('table.itemlist tr td:nth-child(3)').each((i, elem) => {
+      data.push({
+        title : $(elem).text(),
+        link : $(elem).find('a.storylink').attr('href')
+      });
+    });
+    console.log(data);
+  }
+  
+  getData(response.data)
